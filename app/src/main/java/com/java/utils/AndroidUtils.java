2ec1b49d662e48b28f0322d914ea125fb370e735
dhameliyadharmesh <dhameliya7dharmesh@gmail.com>
2022-02-22 21:06:06 +0530

    Add brand name function

--- app/src/main/java/com/java/utils/AndroidUtils.java
+++ app/src/main/java/com/java/utils/AndroidUtils.java
@@ -13,8 +13,23 @@ public class AndroidUtils {
     /**
      * SDK_INT
      * The SDK version of the android OS currently running on this hardware device.
+     * @return OS API version in the int data type
      */
-    public static int getOSVersionSDKINT() {
+    public static int getDeviceOSAPIVersion() {
         return Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * This device model name
+     * @return Device model name in the string object
+     */
+    public static String getDeviceModel() { return Build.MODEL;}
+
+    /**
+     * This device brand name
+     * @return Device brand name in the string object
+     */
+    public static String getDeviceBrand() {
+        return Build.BRAND;
     }
 }
